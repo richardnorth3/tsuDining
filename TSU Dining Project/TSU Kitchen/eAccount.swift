64e1d412ddda6d1c49fcb6f787c19c94dc51ce06
King <richardnorth3@Kings-MacBook-Pro.local>
2016-01-13 02:29:37 -0600

    update 1/13/16

---
+++ TSU Dining Project/TSU Kitchen/eAccount.swift
@@ -0,0 +1,35 @@
+//
+//  eAccount.swift
+//  TSU Dining
+//
+//  Created by King on 1/12/16.
+//  Copyright © 2016 King North III. All rights reserved.
+//
+
+import UIKit
+
+class eAccount: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
